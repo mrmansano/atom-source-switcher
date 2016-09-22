d086916673219a1daf382c8f78500e21d89bf305
Marcelo <marcelo@pumatronix.com.br>
2016-09-22 12:42:38 -0300

    Initial commit

---
+++ spec/atom-source-switcher-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSourceSwitcherView from '../lib/atom-source-switcher-view';
+
+describe('AtomSourceSwitcherView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
